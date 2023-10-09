44a6d29d96737b216221b99a28a0abf9219e1631
Anton Brashkov <spone161@gmail.com>
2023-10-09 13:13:50 +0300

    moved the logic to the App components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Counter} from "./components/counter/Counter";
 
 function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+
+    const minCount: number = 0;
+    const maxCount: number = 5
+
+    const [counter, setCounter] = useState<number>(minCount);
+
+    useEffect(() => {
+        const valueAsString = localStorage.getItem('counterValue')
+
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setCounter(newValue)
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(counter))
+    }, [counter])
+
+    const increaseCounter = () => {
+        counter < maxCount && setCounter(counter + 1)
+    }
+
+    const resetCounter = () => {
+        setCounter(minCount)
+    }
+
+    return (
+        <div className="App">
+            <Counter
+                counter={counter}
+                minCount={minCount}
+                maxCount={maxCount}
+                increaseCounter={increaseCounter}
+                resetCounter={resetCounter}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,22 +1,17 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Scoreboard} from "./scoreboard/Scoreboard";
 import {SectionBtn} from "./sectionBtn/SectionBtn";
 import './style.css'
 
-export const Counter: FC = () => {
+type CounterPropsType = {
+    counter: number
+    minCount: number
+    maxCount: number
+    increaseCounter: () => void
+    resetCounter: () => void
+}
 
-    const minCount: number = 0;
-    const maxCount: number = 5
-
-    const [counter, setCounter] = useState<number>(minCount);
-
-    const increaseCounter = () => {
-        counter < maxCount && setCounter(counter + 1)
-    }
-
-    const resetCounter = () => {
-        setCounter(minCount)
-    }
+export const Counter: FC<CounterPropsType> = ({counter, minCount, maxCount, increaseCounter, resetCounter}) => {
 
     return (
         <div className='wrapper-counter'>
